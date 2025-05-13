73a91fc2bf521026f09fd40567b829f3d538f263
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-05-13 08:42:11 -0400

    created the 1st example of inheritance

---
+++ BookExample/src/main/java/com/pluralsight/Animal.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class Animal {
+    private String name;
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+}
